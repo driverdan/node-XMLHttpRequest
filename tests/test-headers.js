93a54c550fa579bba6a946ed40f4da19ed82b54c
driverdan <dan@driverdan.com>
2009-10-04 13:15:59 -0400

    Added headers test file, updated readme with todos.

---
+++ tests/test-headers.js
@@ -0,0 +1,36 @@
+include("common.js");
+include("/http.js");
+
+var xhr;
+
+// Test server
+var server = createServer(function (req, res) {
+	// Test setRequestHeader
+	assertEquals("Foobar", req.headers["X-Test"]);
+	
+	var body = "Hello World";
+	res.sendHeader(200, {
+		"Content-Type": "text/plain",
+		"Content-Length": body.length
+	});
+	res.sendBody("Hello World");
+	res.finish();
+	
+	this.close();
+}).listen(8000);
+
+xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+	if (this.readyState == 4) {
+		// Test getAllResponseHeaders()
+		var headers = "Content-Type: text/plain\r\nContent-Length: 11\r\nConnection: close";
+		assertEquals(headers, this.getAllResponseHeaders());
+		
+		puts("done");
+	}
+};
+
+xhr.open("GET", "http://localhost:8000/");
+xhr.setRequestHeader("X-Test", "Foobar");
+xhr.send();
