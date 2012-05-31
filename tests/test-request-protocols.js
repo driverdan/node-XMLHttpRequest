725ab0be016d78275f7403ae692e3bab9dfc5b4a
Dan DeFelippi <dan@driverdan.com>
2012-05-31 09:29:40 -0500

    Add support for file protocol.
    Add support for file:// protocol to load local files. Code taken from
    https://github.com/liucougar/node-XMLHttpRequest/commit/31a638a646fe58de20b4e5c50b22b28d69e1ef24
    
    Includes basic test which currently fails due to extra \n in response.
    Issue with readFile?

---
+++ tests/test-request-protocols.js
@@ -0,0 +1,17 @@
+var sys = require("util")
+  ,assert = require("assert")
+  ,XMLHttpRequest = require("../lib/XMLHttpRequest").XMLHttpRequest
+  ,xhr;
+
+xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+  if (this.readyState == 4) {
+    assert.equal("Hello World", this.responseText);
+    this.close();
+  }
+};
+
+var url = "file://" + __dirname + "/testdata.txt";
+xhr.open("GET", url);
+xhr.send();
