10285e46058e102a3bac4a3c83251d21b09bae12
Clóvis Lima <clovis.junior@gmail.com>
2016-10-13 11:19:15 -0300

    Added the constants to both prototype and class itself

--- tests/test-constants.js
+++ tests/test-constants.js
@@ -3,7 +3,14 @@ var sys = require("util")
   , XMLHttpRequest = require("../lib/XMLHttpRequest").XMLHttpRequest
   , xhr = new XMLHttpRequest();
 
-// Test constant values
+// Test constant values in class
+assert.equal(0, XMLHttpRequest.UNSENT);
+assert.equal(1, XMLHttpRequest.OPENED);
+assert.equal(2, XMLHttpRequest.HEADERS_RECEIVED);
+assert.equal(3, XMLHttpRequest.LOADING);
+assert.equal(4, XMLHttpRequest.DONE);
+
+// Test constant values in instance
 assert.equal(0, xhr.UNSENT);
 assert.equal(1, xhr.OPENED);
 assert.equal(2, xhr.HEADERS_RECEIVED);
